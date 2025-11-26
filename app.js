742b1b4b17700f28359e774b234d46d477c50770
LuisoDev1 <luisbet92@gmail.com>
2025-11-26 06:01:14 +0000

    agregando test para la funsion suma

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-
-console.log("Hello World");
+const saludo = (mensaje) => {
+    console.log(mensaje);
+    return mensaje;
+}
 
 const sum = (a,b) => {
     return a + b;
 }
 
 console.log(sum(7,3));
+console.log("hola");
 
 
-module.exports = {sum};
+module.exports = {sum, saludo};
--- test.js
+++ test.js
@@ -1,5 +1,13 @@
 
+const { saludo } = require('./app.js');
+
 const { sum } = require('./app.js');
+
+test("la funsion debe devolver hola", () => {
+    let miSaludo = saludo("hola");
+
+    expect(miSaludo).toBe("hola");
+})
 
 test("add 14 + 9 to equal 23",() => {
     let total = sum(14,9);
